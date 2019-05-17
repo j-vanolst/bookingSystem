3ac6b9a03a4799c97b52c9a13b2d6098177beca3
Jos van Olst <vanolstj@gmail.com>
2019-05-17 19:01:01 +1200

    shit

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
   define('DB_SERVER', 'localhost');
   define('DB_USERNAME', 'root');
-  define('DB_PASSWORD', 'Qjxcafnllrwluxru1');
+  define('DB_PASSWORD', '##########');
   define('DB_NAME', 'bookingsystem');
 
   $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
